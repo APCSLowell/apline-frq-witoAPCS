438216cd5458158cb1d05803d69a2bbbd5b030f3
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-24 18:37:37 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,19 @@
 public class APLine
 {
   /* your code here */
+  private int a , b , c;
+ public APLine(int a, int b, int c) {
+ this.a = a;
+ this.b = b;
+ this.c = c;
+ }
+
+ public double getSlope() {
+ return ( - (this.a / (double) this.b));
+ }
+
+  public boolean isOnLine(int x, int y) {
+ return (0 == (this.a * x) + (this.b * y) + this.c);
+ }
   
 }
